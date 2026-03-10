a3e45b14e3bc66c746c25c1f2f9cc3d7314f7593
marta <martagp2332007@gmail.com>
2026-03-10 16:41:02 +0100

    fin

--- S10/echo-server3.py
+++ S10/echo-server3.py
@@ -2,17 +2,19 @@ import socket
 ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 PORT = 8080
-IP = "172.20.10.4"
+IP = "212.128.255.92"
 ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 ls.bind((IP, PORT))
 ls.listen()
 print("The server is configured!")
 connection_count = 0
+client_list = []
 while True:
     print("Waiting for Clients to connect")
     (cs, client_ip_port) = ls.accept()
     connection_count += 1
+    client_list.append(client_ip_port)
     print("A client has connected to the server!")
     msg_raw = cs.recv(2048)
     msg = msg_raw.decode()
@@ -21,3 +23,13 @@ while True:
     response = f"ECHO:{msg}\n"
     cs.send(response.encode())
     cs.close()
+
+    #voy a poner un limite de 5 porque si lo tengo q parar yo me da error y me sale todo rojo
+    if connection_count == 5:
+        print("The total clients connected is:\n")
+        for client in client_list:
+            print(client)
+
+        print("Finished")
+        break
+ls.close()
