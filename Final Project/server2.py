a0cf7ad3b24a2dfd71251f0826a60c38df41941c
Marta G París <martagp2332007@gmail.com>
2026-05-06 15:00:57 +0200

    final project. update; he medio hecho el medium level, tengo que mejorar y revisar e intentar hacer el ultimo
    Signed-off-by: Marta G París <martagp2332007@gmail.com>

--- Final Project/server2.py
+++ Final Project/server2.py
@@ -90,7 +90,68 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                 contents = Path('html/error.html').read_text().replace("{{message}}", "Error en los datos")
                 error_code = 404
 
+            #MEDIUM LEVEL#
+            # CASO 5: /geneLookup (Obtener el ID ENSG de un nombre de gen)
+            elif resource == "/geneLookup":
+            gene_name = params.split("=")[1]
+            # API: /lookup/symbol/homo_sapiens/NOMBRE_GEN
+            conn = http.client.HTTPSConnection("rest.ensembl.org")
+            conn.request("GET", "/lookup/symbol/homo_sapiens/" + gene_name, headers={"Content-Type": "application/json"})
+            res = conn.getresponse()
 
+            if res.status == 200:
+                data = json.loads(res.read().decode())
+                gene_id = data['id']  # Esto nos da el ENSG...
+                plantilla = Path('html/geneLookup.html').read_text()
+                contents = plantilla.replace("{{gene}}", gene_name).replace("{{id}}", gene_id)
+            else:
+                contents = Path('html/error.html').read_text().replace("{{message}}", "Gene not found")
+                error_code = 404
+
+        # CASO 6: /geneSeq (Obtener la secuencia de ADN dado el nombre)
+        elif resource == "/geneSeq":
+        gene_name = params.split("=")[1]
+        # PASO 1: Buscar el ID (igual que en el caso anterior)
+        conn = http.client.HTTPSConnection("rest.ensembl.org")
+        conn.request("GET", "/lookup/symbol/homo_sapiens/" + gene_name, headers={"Content-Type": "application/json"})
+        res = conn.getresponse()
+
+        if res.status == 200:
+            gene_id = json.loads(res.read().decode())['id']
+            # PASO 2: Con el ID, pedir la secuencia
+            # API: /sequence/id/ENSG...
+            conn.request("GET", "/sequence/id/" + gene_id, headers={"Content-Type": "application/json"})
+            res2 = conn.getresponse()
+            seq_data = json.loads(res2.read().decode())
+
+            plantilla = Path('html/geneSeq.html').read_text()
+            contents = plantilla.replace("{{gene}}", gene_name).replace("{{sequence}}", seq_data['seq'])
+        else:
+            contents = "Error obteniendo secuencia"
+            error_code = 404
+
+    # CASO 7: /geneInfo (Detalles del gen)
+    elif resource == "/geneInfo":
+        gene_name = params.split("=")[1]
+        conn = http.client.HTTPSConnection("rest.ensembl.org")
+        conn.request("GET", "/lookup/symbol/homo_sapiens/" + gene_name,
+                     headers={"Content-Type": "application/json"})
+        res = conn.getresponse()
+
+        if res.status == 200:
+            data = json.loads(res.read().decode())
+            plantilla = Path('html/geneInfo.html').read_text()
+            # Calculamos la longitud restando final e inicio
+            length = data['end'] - data['start']
+
+            contents = plantilla.replace("{{id}}", data['id']) \
+                .replace("{{chromo}}", data['seq_region_name']) \
+                .replace("{{start}}", str(data['start'])) \
+                .replace("{{end}}", str(data['end'])) \
+                .replace("{{length}}", str(length))
+        else:
+            contents = "Gene info not found"
+            error_code = 404
 
         #error max
         else:
