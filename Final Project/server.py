bf21e3bdc701424270dad4d37572034581c8cb24
Marta G París <martagp2332007@gmail.com>
2026-05-19 11:51:34 +0200

    final project ACABADOOOO!!!
    Signed-off-by: Marta G París <martagp2332007@gmail.com>

--- Final Project/server.py
+++ Final Project/server.py
@@ -20,7 +20,7 @@ def seq_percent(seq):
 #
 
 class TestHandler(http.server.BaseHTTPRequestHandler):
-    def do_GET(self): #para GET el request, basicament, parteix el missatge pa llegir lo que es rsource i request i tal
+    def do_GET(self):
         list_resource = self.path.split('?')
         resource = list_resource[0]
         params = ""
@@ -45,21 +45,18 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                 if "limit=" in params:
                     limite = int(params.split("=")[1])
                     specie = specie[:limite]
-                # Creamos el trozo de HTML con los nombres
                 list_html = ""
                 for e in specie:
                     list_html += "<li>" + e['display_name'] + "</li>"
-
-                # Cargamos la plantilla y cambiamos el "hueco" por nuestra lista
                 page = Path('html/listSpecies.html').read_text()
                 contents = page.replace("{{list}}", list_html)
             else:
-                contents = "Error al conectar con Ensembl"
+                contents = "Error connecting with Ensembl"
                 error_code = 500
 
         #3: karyo
         elif resource == "/karyotype":
-            specie = params.split("=")[1] # Sacamos el nombre de la especie del parámetro (ej: species=human)
+            specie = params.split("=")[1]
             conn = http.client.HTTPSConnection("rest.ensembl.org")
             conn.request("GET", "/info/assembly/" + specie, headers={"Content-Type": "application/json"})
             response = conn.getresponse()
@@ -87,8 +84,7 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
             if response.status == 200:
                 data = json.loads(response.read().decode())
                 long = "not found"
-                # Buscamos en la lista de regiones la que coincida con el nombre del cromosoma
-                for region in data['top_level_region']:
+                for region in data['top_level_region']: #searching in dif reg for the chromo name
                     if region['name'] == chromosome:
                         long = str(region['length'])
                         break
@@ -96,10 +92,10 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                 page = Path('html/chromosomeLength.html').read_text()
                 contents = page.replace("{{species}}", specie).replace("{{chromo}}", chromosome).replace("{{length}}", long)
             else:
-                contents = Path('html/error.html').read_text().replace("{{message}}", "Error en los datos")
+                contents = Path('html/error.html').read_text().replace("{{message}}", "Error in the data")
                 error_code = 404
 
-            #MEDIUM LEVEL#
+#MEDIUM LEVEL#
         #5: /geneLookup (ID)
         elif resource == "/geneLookup":
             gene_name = params.split("=")[1]
@@ -119,15 +115,13 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
 
         # CASO 6: /geneSeq dado nombre
         elif resource == "/geneSeq":
-            gene_name = params.split("=")[1] # para buscar n
+            gene_name = params.split("=")[1] #input name
             conn = http.client.HTTPSConnection("rest.ensembl.org")
             conn.request("GET", "/lookup/symbol/homo_sapiens/" + gene_name, headers={"Content-Type": "application/json"})
             response = conn.getresponse()
 
             if response.status == 200: #AQUI TENGO QUE MIRAR PORQUE NO SE PORQUE TENGO Q OBTENER DOS RESPUESTAS DIFERENTES
-                gene_id = json.loads(response.read().decode())['id']
-                # PASO 2: Con el ID, pedir la secuencia
-                # API: /sequence/id/ENSG...
+                gene_id = json.loads(response.read().decode())['id'] #sacamos id xq necesitamos la secuencia
                 conn.request("GET", "/sequence/id/" + gene_id, headers={"Content-Type": "application/json"})
                 response2 = conn.getresponse()
                 seq_data = json.loads(response2.read().decode())
@@ -135,7 +129,7 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                 page = Path('html/geneSeq.html').read_text()
                 contents = page.replace("{{gene}}", gene_name).replace("{{sequence}}", seq_data['seq'])
             else:
-                contents = "Error obteniendo secuencia"
+                contents = "Error found in sequence"
                 error_code = 404
 
         #7: /geneInfo
@@ -148,7 +142,6 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
             if response.status == 200:
                 data = json.loads(response.read().decode())
                 page = Path('html/geneInfo.html').read_text()
-                # Calculamos la longitud restando final e inicio
                 length = data['end'] - data['start']
 
                 contents = page.replace("{{id}}", data['id']) \
@@ -160,7 +153,7 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                 contents = "Gene info not found"
                 error_code = 404
 
-        #8: longitud porcentaje
+        #8: longitud porcentaje (tngo q volver a sacar id cn nombre para acceder a info...)
         elif resource == "/geneCalc":
             gene_name = params.split("=")[1]
             conn = http.client.HTTPSConnection("rest.ensembl.org")
@@ -176,9 +169,9 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                 percents = str(seq_percent(full_seq))
 
                 page = Path('html/geneCalc.html').read_text()
-                contents = page.replace("{{length}}", length).replace("{{percents}}", percents)
+                contents = page.replace("{{gene}}", gene_name).replace("{{length}}", length).replace("{{percents}}", percents)
             else:
-                contents = "Error obteniendo secuencia"
+                contents = "Error obtaining the sequence"
                 error_code = 404
         #9: /geneList (gnes en reg)
         elif resource == "/geneList":
@@ -186,7 +179,7 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
             chromo = ""
             start = ""
             end = ""
-            for part in parts:
+            for part in parts: #otra manera d partir cndo tngo muchos params xq sino me lio
                 if part.startswith("chromo="):
                     chromo = part.split("=")[1]
                 elif part.startswith("start="):
